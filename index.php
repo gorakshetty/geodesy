3fa350c75e11146241bf1cd3d14aedc4bf91b699
Gorak Shetty <gorak_shetty@yahoo.com>
2023-08-15 16:20:46 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Geodesy Mangalore</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#myPage"><img src="images/logo.png" class="logo-image" alt="Paris" height="50"><img src="images/header.png"  class="logo-image" alt="Paris" height="50"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#services">SERVICES</a></li>
+        <li><a href="#portfolio">PORTFOLIO</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron text-center">
+  <h1>Geodesy Mangalore</h1> 
+  <p>Innovation and Excellence</p> 
+    <div class="input-group">
+      <div class="input-group-btn">
+      <a href="#contact"><button type="button" class="btn btn-danger">Contact Us</button></a>
+      </div>
+    </div>
+</div>
+
+<!-- Container (About Section) -->
+<div id="about" class="container-fluid">
+  <div class="row">
+    <div class="col-sm-8">
+      <h2>About Geodesy Mangalore</h2><br>
+      <h4>Geodesy Mangalore. is incorporated under the companies Act, 2013 (18 of 2013).
+Offices are located at Mangaluru and Udupi.
+Operational since 2020.</h4>
+<p>Specialties include but not limited to, Cadastral Surveying including Remote Sensing & Geographical Information System, Civil Engineering, Software Development, Research & Development etc.
+</p>
+     <br><a href="#contact"><button class="btn btn-default btn-lg">Get in Touch</button></a>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-globe logo"></span>
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid bg-grey">
+  <div class="row">
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-leaf logo slideanim"></span>
+    </div>
+    <div class="col-sm-8">
+      <h2>Our Values</h2><br>
+      <h4><strong>MISSION:</strong> Every piece of work is a step to reaching some larger objective. We deliver work keeping in mind the customers end objective. Freedom, Passion, common sense and skill are our levers ; integrity and commitment our principle.</h4><br>
+      <p><strong>VISION:</strong> "Only when the last tree has died, and the last river been poisoned, and the last fish been caught will we realize we cannot eat money"  - The  Red Indian Proverb
+ </p>
+    </div>
+  </div>
+</div>
+
+<!-- Container (Services Section) -->
+<div id="services" class="container-fluid text-center">
+  <h2>SERVICES</h2>
+  <h4>What we offer</h4>
+  <br>
+  <div class="row slideanim">
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-flag logo-small"></span>
+      <h4>Surveys</h4>
+      <p>Cadastral , Revenue Survey and Contour survey</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-map-marker logo-small"></span>
+      <h4>Mapping</h4>
+      <p>Forest & Vegetation Mapping</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-stats logo-small"></span>
+      <h4>Analysis</h4>
+      <p>Farming & Crop Pattern</p>
+    </div>
+  </div>
+  <br><br>
+  <div class="row slideanim">
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-book logo-small"></span>
+      <h4>Studies</h4>
+      <p>Coastal and Ocean Studies</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-facetime-video logo-small"></span>
+      <h4>Lidar</h4>
+      <p> LIDAR in Terrain Modeling & Stockpile Monitoring </p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-plane logo-small"></span>
+      <h4 style="color:#303030;">Drone Survey</h4>
+      <p>Drone in Cadastral Survey</p>
+    </div>
+  </div>
+</div>
+
+<!-- Container (Portfolio Section) -->
+<div id="portfolio" class="container-fluid text-center bg-grey">
+  <h2>Portfolio</h2><br>
+  <div class="row text-center slideanim">
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="images/map.png" alt="Paris" width="400" height="300">
+        <p><strong>Mapping</strong></p>
+        
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="images/drone.png" alt="New York" width="400" height="300">
+        <p><strong>Drone Survey</strong></p>
+       
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="images/survey.png" alt="San Francisco" width="400" height="300">
+        <p><strong>Surveying</strong></p>
+       
+      </div>
+    </div>
+  </div><br>
+  
+  <h2> our Technology</h2>
+  <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+      <li data-target="#myCarousel" data-slide-to="4"></li>
+
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+      <div class="row">
+       <img src="images/image1.png" alt="Drone in Agriculture"  height="200">
+        <h4> Drone in Agriculture </h4>
+      </div>
+      </div>
+      <div class="item">
+      <div class="row">
+      <img src="images/image2.png" alt="LIDAR in Cut fill aspects"  height="200">
+        <h4> Drone &amp; LIDAR in Cut fill aspects</h4>
+      </div>
+      </div>
+      <div class="item">
+      <div class="row">
+      <img src="images/image3.png" alt="LIDAR -Thermal Imaging Survey" height="200">
+        <h4>Drone &amp; LIDAR -Thermal Imaging Survey </h4>
+      </div>
+      </div>
+      <div class="item">
+      <div class="row">
+      <img src="images/image4.png" alt="Reconstruction" height="200">
+        <h4>Drone in 3D Mapping &amp; Reconstruction</h4>
+       </div>
+      </div>
+      <div class="item">
+      <div class="row">
+      <img src="images/image5.png" alt="Volumetric Survey" height="200">
+        <h4>Volumetric Survey</h4>
+      </div>
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+<!-- Container (Contact Section) -->
+<div id="contact" class="container-fluid bg-grey">
+  <h2 class="text-center">CONTACT</h2>
+  <div class="row">
+    <div class="col-sm-5">
+      <p>Contact us and we'll get back to you within 24 hours.</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span> Geodesy Mangalore</p>
+      <p><span class="glyphicon glyphicon-phone"></span> +91-9449818550, +91-9886761192, +91-9663043080</p>
+      <p><span class="glyphicon glyphicon-envelope"></span> kavoorlab@gmail.com</p>
+    </div>
+    <div class="col-sm-7 slideanim">     
+      <div class="row">
+        <div class="col-sm-12 form-group">
+        <div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Sai grandeur B &amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://connectionsgame.org/">Connections NYT</a></div><style>.mapouter{position:relative;text-align:right;width:600px;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:600px;height:400px;}.gmap_iframe {width:600px!important;height:400px!important;}</style></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Image of location/map -->
+
+
+<footer class="container-fluid text-center">
+<p>&copy; Copyright 2008-2023 Geodesy Mangalore · All Rights Reserved.</p>
+  <a href="#myPage" title="To Top">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+  
+</footer>
+
+<script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
+</script>
+
+</body>
+</html>
